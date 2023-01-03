758f0cd3d6e521beda6e053649b6cc30ac22ab04
Dongjoon Lee <djunnni@gmail.com>
2023-01-04 00:18:00 +0900

    blog: 2022년 회고 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,13 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "djunnni", // Usually your GitHub org/user name.
-  projectName: "djunnni.github.io", // Usually your repo name.
+
+  organizationName: "djunnni",
+  projectName: "djunnni.github.io",
   trailingSlash: false,
 
   deploymentBranch: "gh-pages",
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -38,6 +35,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/djunnni/djunnni.github.io/tree/main/",
+          exclude: ["tutorial-basics", "tutorial-extras"],
         },
         blog: {
           showReadingTime: true,
