1b04b99bb769b30076098ba2ee9abca06fbcc1c2
Dongjoon Lee <djunnni@gmail.com>
2023-01-15 19:28:41 +0900

    필요없는 doc 삭제

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/djunnni/djunnni.github.io/tree/main/",
-          exclude: ["tutorial-basics", "tutorial-extras"],
+          exclude: [],
         },
         blog: {
           showReadingTime: true,
